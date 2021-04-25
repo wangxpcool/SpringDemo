1dc616dc09bd1ee7dc64dff6c520daebf07915a0
wangxpcool <wangxpcool@qq.com>
2021-04-25 23:01:57 +0800

    Create DemoApplication.java

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+
+@MapperScan(value = {"com.example.demo.mapper"})
+
+
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+}
