f877a35159ee974366e2c9c4427115a0d97b47d0
wangxpcool <wangxpcool@qq.com>
2021-04-25 23:02:01 +0800

    Create StuImp.java

---
+++ src/main/java/com/example/demo/service/iml/StuImp.java
@@ -0,0 +1,15 @@
+//package com.example.demo.service.iml;
+//
+//import com.example.demo.mapper.StuMapper;
+//import com.example.demo.service.Stu;
+//import org.springframework.beans.factory.annotation.Autowired;
+//
+//public class StuImp implements Stu {
+//
+//    @Autowired
+//    StuMapper stuMapper;
+//
+//    public String list(){
+//        return stuMapper.list();
+//    }
+//}
